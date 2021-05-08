4e0aa7aa451429b7c8f4ea744db7dee19d282e20
Alan Sparremberger <alan.rs@outlook.com>
2021-05-08 02:18:04 -0300

    Kek

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,13 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
+
+app.get('/qq', (req, res) => {
+  res.send('Jura né');
+});
 app.listen(port, err => {
   if (err) {
-    return console.error(err);
+    return console.log(err);
   }
   return console.log(`server is listening on ${port}`);
 });

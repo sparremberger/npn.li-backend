8de8a29c900620cc257cbbc5b514b32590a03798
Alan Sparremberger <alan.rs@outlook.com>
2021-05-17 11:32:29 -0300

    Funcionando

--- dist/app.js
+++ dist/app.js
@@ -4,23 +4,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path = require('path');
+const path = require("path");
 const app = express_1.default();
 const port = 3000;
-app.use(express_1.default.static(path.join(__dirname, 'alanspa')));
-app.get('/', (req, res) => {
-    res.send(`oloko`);
+/*app.get("/", (req: Request, res: Response) => {
+    console.log("teste!!");
+    console.log(__dirname);
 });
-app.get('/api', (req, res) => {
-    res.send('Eita');
+
+app.get("/api", (req: Request, res: Response) => {
+    res.send("Eita");
     console.log(req.query);
 });
-app.get('/api/:oi', (req, res) => {
-    res.send('Eita');
+
+app.get("/api/:oi", (req: Request, res: Response) => {
+    res.send("Eita");
     console.log(req.params);
     console.log(req.url);
     console.log(req.body);
 });
+*/
+app.get('/', (req, res) => {
+    console.log("Get /");
+    res.sendFile('./alanspa/index.html', { root: __dirname });
+});
+app.get('/projetos', (req, res) => {
+    console.log("Get /projetos");
+    res.sendFile('./alanspa/projetos.html', { root: __dirname });
+});
+app.get('/galeria', (req, res) => {
+    console.log("Get /galeria");
+    res.sendFile('./alanspa/galeria.html', { root: __dirname });
+});
+app.use(express_1.default.static('dist/alanspa'));
 app.listen(port, (err) => {
     if (err != null) {
         return console.log(err);
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,49 @@
-import express, { Request, Response } from 'express';
-const path = require('path');
+import express, { Request, Response } from "express";
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'alanspa')));
-
-app.get('/', (req : Request, res : Response) => {
-  res.send(`oloko`);
+/*app.get("/", (req: Request, res: Response) => {
+    console.log("teste!!");
+    console.log(__dirname);
 });
 
-app.get('/api', (req : Request, res : Response) => {
-  res.send('Eita');
-  console.log(req.query)
+app.get("/api", (req: Request, res: Response) => {
+    res.send("Eita");
+    console.log(req.query);
 });
 
-app.get('/api/:oi', (req : Request, res : Response) => {
-    res.send('Eita');
-    console.log(req.params)
+app.get("/api/:oi", (req: Request, res: Response) => {
+    res.send("Eita");
+    console.log(req.params);
     console.log(req.url);
     console.log(req.body);
-  });
+});
+*/
+app.get('/', (req : Request, res : Response) => {
+    console.log("Get /");
+    res.sendFile('./alanspa/index.html', { root: __dirname });
 
-app.listen(port, (err : void) => {
-  if (err != null) {
-    return console.log(err);
-  }
-  return console.log(`server is listening on ${port}`);
+});
+
+app.get('/projetos', (req : Request, res : Response) => {
+    console.log("Get /projetos");
+    res.sendFile('./alanspa/projetos.html', { root: __dirname });
+
+});
+
+app.get('/galeria', (req : Request, res : Response) => {
+    console.log("Get /galeria");
+    res.sendFile('./alanspa/galeria.html', { root: __dirname });
+
+});
+
+app.use(express.static('dist/alanspa'));
+
+
+app.listen(port, (err: void) => {
+    if (err != null) {
+        return console.log(err);
+    }
+    return console.log(`server is listening on ${port}`);
 });

45c4c37a50ea0051561b814c9370d3a03dea7574
Alan Sparremberger <alan.rs@outlook.com>
2021-05-24 21:40:35 -0300

    ha

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 const User = require('../models/User');
 
 function findUser(user : any, req : Request, res : Response) {
-    User.findOne({ email: "av@a" }, function (err: any, result: any) {
+    User.findOne({ email: "np@a" }, function (err: any, result: any) {
         if (err) {
             res.send(err);
         } else {
